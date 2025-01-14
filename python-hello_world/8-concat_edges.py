6bc781d9d61581877454004ab407391ffb0d87a5
Damien353 <10034@holbertonstudents.com>
2025-01-14 13:16:47 +0000

    premier test

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
+part1 = str[39:66]
+part2 = " with Python"
+print(part1 + part2)
