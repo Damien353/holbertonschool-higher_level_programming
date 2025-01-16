16715a6d1dd0705908ad5437ea5e59e17c855ed8
Damien353 <10034@holbertonstudents.com>
2025-01-16 16:08:34 +0000

    modif .format

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 for i in range(10):  # parcourt les chiffres de 0 a 9
+    
     for j in range(i + 1, 10):  # parcourt chiffres plus grands que i
+        
         if i == 8 and j == 9:  # si derniere combinaison
-            print(f"{i}{j}")
+            print("{}{}".format(i, j))
         else:
-            print(f"{i}{j}", end=", ")
+            print("{}{}".format(i, j), end=", ")
