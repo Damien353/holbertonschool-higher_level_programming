f1eae78231024157135edb8241ee7d6f90e82fbf
Damien353 <10034@holbertonstudents.com>
2025-01-16 17:56:29 +0000

    switch upper

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for ch in str:
+        if ord(ch) => 97 and ord(ch) <= 122:
+            result += chr(ord(ch) -32)
+        else:
+            result += ch
+            print("{}".format(result))
