0728a482effa6424f7e191025c2c0d69b30f170f
Damien353 <10034@holbertonstudents.com>
2025-01-16 16:13:13 +0000

    white space en trop

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 for i in range(10):  # parcourt les chiffres de 0 a 9
-    
+
     for j in range(i + 1, 10):  # parcourt chiffres plus grands que i
-        
+
         if i == 8 and j == 9:  # si derniere combinaison
             print("{}{}".format(i, j))
         else:
