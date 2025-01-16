2c1e699ec6ae93077f5023037d67dd594719a683
Damien353 <10034@holbertonstudents.com>
2025-01-16 14:00:49 +0000

    modif

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(97, 123):  # 97 code ASCII de a, 123 code ASCII de z + 1
-    print("{}".format(chr(i)), end="")  # convertit en caractere et evite saut de ligne
+    print("{}".format(chr(i)), end="")  # convertit
