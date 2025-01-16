694d54957613a40047f28b01417ee31618796133
Damien353 <10034@holbertonstudents.com>
2025-01-16 17:15:47 +0000

    indentation

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     conversion = ord(c)
-        if conversion in range(97, 123):
-            return True
-        else:
-            return False
+    if conversion in range(97, 123):
+        return True
+    else:
+        return False
