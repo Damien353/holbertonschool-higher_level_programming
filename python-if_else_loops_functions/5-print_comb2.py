12470b0d5482f618dbf5c0560615e63b08fe3a9f
Damien353 <10034@holbertonstudents.com>
2025-01-16 13:55:58 +0000

    test ajout d

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for i in range(0, 99):
     if i < 98:
-        print("{:02}".format(i), end=", ")
+        print("{:02d}".format(i), end=", ")
     else:
-        print("{:02}".format(i))
+        print("{:02d}".format(i))
