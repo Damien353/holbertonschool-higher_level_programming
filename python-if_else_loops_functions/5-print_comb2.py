e80b830e263f22a3c08436d5965ddf2c4069f607
Damien353 <10034@holbertonstudents.com>
2025-01-16 13:54:26 +0000

    test

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for i in range(0, 99):
     if i < 98:
-        print("{:02}".format(i), end=", ")  # Affiche les nombres avec 2 chiffres, sauf pour 99
+        print("{:02}".format(i), end=", ")
     else:
-        print("{:02}".format(i)) #Pour afficher 99 sans ,
+        print("{:02}".format(i))
