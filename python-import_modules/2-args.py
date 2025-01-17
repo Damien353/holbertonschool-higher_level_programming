5289a5a8893f87a438cbcfa5b758be7fc61936e4
Damien353 <10034@holbertonstudents.com>
2025-01-17 12:57:19 +0000

    sys.argv

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    arguments = len(argv)
+    arguments = len(sys.argv)
     arguments_count = arguments - 1
     if arguments_count == 0:
         print("0 arguments.")
