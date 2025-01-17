7a20aff6e1e8e3dfc6cc8d137af621c1bb96c1e0
Damien353 <10034@holbertonstudents.com>
2025-01-17 09:31:32 +0000

    supr commentaires

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,15 +2,11 @@
 
 def fizzbuzz():
     for i in range(1, 101):
-        # ve©rifier les multiples de 3 et 5 (FizzBuzz)
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
-        # ve©rifier les multiples de 3(Fizz)
         elif i % 3 == 0:
             print("Fizz", end=" ")
-        # ve©rifier les multiples de 5 (Buzz)
         elif i % 5 == 0:
             print("Buzz", end=" ")
-        # sinon imprime le nombre lui meme
         else:
             print("{}".format(i), end=" ")
