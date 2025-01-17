fbd0c8146adcc55ae45fdb27f6c180910d8ebc09
Damien353 <10034@holbertonstudents.com>
2025-01-17 12:50:18 +0000

    print arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    arguments = len(argv)
+    arguments_count = arguments - 1
+    if arguments_count == 0:
+        print("0 arguments.")
+    elif arguments_count == 1:
+        print("1 argument :")
+    else:
+        print("{} arguments :".format(arguments_count))
+    for i in range(1, arguments):
+        print("{}: {}".format(i, sys.argv[i]))
