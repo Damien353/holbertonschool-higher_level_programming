13ffb431499b90404fcfafa9dcdeb3162d4be61a
Damien353 <10034@holbertonstudents.com>
2025-01-20 11:20:08 +0000

    code fonctionnel

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:  # parcourt chaque ligne
-        print(" ".join("{:d}".format(num) for num in row), end="")
-        print("$")
+        print(" ".join("{:d}".format(num) for num in row))
