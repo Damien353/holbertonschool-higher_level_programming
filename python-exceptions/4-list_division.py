e96a704711c14d583bb74a750740d781b86e67f8
Damien353 <10034@holbertonstudents.com>
2025-01-22 14:22:38 +0000

    rajout condition if avec code fonctionnel

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = []
-    for i in range(min(len(my_list_1), len(my_list_2), list_length)):
+    for i in range(list_length):
         try:
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                raise IndexError
+
             if (not isinstance(my_list_1[i], (int, float)) or 
                 not isinstance(my_list_2[i], (int, float))):
                 raise TypeError
