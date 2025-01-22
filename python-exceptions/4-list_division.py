282f18b2c41736c0e5675be6d3d8e236f06e9277
Damien353 <10034@holbertonstudents.com>
2025-01-22 14:18:17 +0000

    remodelage du code

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = []
-
-    for i in range(list_length):
+    for i in range(min(len(my_list_1), len(my_list_2), list_length)):
         try:
-            if i >= len(my_list_1) or i >= len(my_list_2):
-                raise IndexError
-            if not isinstance(my_list_1[i], (int, float)):
+            if (not isinstance(my_list_1[i], (int, float)) or 
+                not isinstance(my_list_2[i], (int, float))):
                 raise TypeError
-            if not isinstance(my_list_2[i], (int, float)):
-                raise TypeError
-
-            result.append(my_list_1[i] / my_list_2[i])
             division_result = my_list_1[i] / my_list_2[i]
-
         except ZeroDivisionError:
+            division_result = 0
             print("division by 0")
-            result.append(0)
         except TypeError:
+            division_result = 0
             print("wrong type")
-            result.append(0)
         except IndexError:
+            division_result = 0
             print("out of range")
-            result.append(0)
         finally:
-            if 'division_result' not in locals():
-                division_result = 0
             result.append(division_result)
     return result
