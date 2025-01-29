54e0a180f90ccbbd4944f8f42f8636de51751b61
Damien353 <10034@holbertonstudents.com>
2025-01-29 13:47:22 +0000

    ajout description

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+"""
+Module that defines a Rectangle class.
+
+This module contains the definition of a Rectangle class.
+Currently, the class is empty, serving as a placeholder for future implementations.
+"""
+
+
 class Rectangle:
+    """
+    A class that defines a rectangle.
+
+    This class is currently empty and serves as a base for future extensions.
+    """
     pass
