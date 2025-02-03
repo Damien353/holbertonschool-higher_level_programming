87ce93768e054efc9a06d73cfabe21043e4d8a28
Damien353 <10034@holbertonstudents.com>
2025-02-03 14:52:22 +0000

    code fonctionnel

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module contenant une classe vide nommee BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Une classe de base vide pour la geometrie.
+    """
+    pass
