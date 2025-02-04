83cc37800f2b88e58174f08becbb0c0a37256d09
Damien353 <10034@holbertonstudents.com>
2025-02-04 10:01:43 +0000

    code fonctionnel

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module qui contient la classe Rectangle, qui herite de la classe BaseGeometry.
+Cette classe permet de creer un rectangle en validant ses dimensions.
+Elle inclut une methode pour calculer l'aire du rectangle et
+une representation sous forme de chaine.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Classe Rectangle qui herite de la classe BaseGeometry.
+    Elle represente un rectangle et valide les dimensions de ce
+    rectangle (largeur et hauteur).
+    """
+
+    def __init__(self, width, height):
+        """
+        Constructeur de la classe Rectangle.
+
+        Args:
+            width (int): La largeur du rectangle.
+            height (int): La hauteur du rectangle.
+        Leve:
+            TypeError: Si la largeur ou la hauteur ne sont pas des entiers.
+            ValueError: Si la largeur ou la hauteur sont inferieures ou
+            egales a 0.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
