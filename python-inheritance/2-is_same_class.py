12d8d98dd4a9933b5af0a8d58383060a45be92a2
Damien353 <10034@holbertonstudents.com>
2025-02-06 08:43:37 +0000

    supr ==

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -14,4 +14,4 @@ def is_same_class(obj, a_class):
     Returns:
         bool: True si obj est une instance exacte de a_class, False sinon.
     """
-    return type(obj) == is a_class
+    return type(obj) is a_class
