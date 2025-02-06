b4cbf51c0705e7354467b832ebf26dd5f9e6ebbc
Damien353 <10034@holbertonstudents.com>
2025-02-06 21:25:03 +0000

    modif return

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -2,17 +2,14 @@
 from abc import ABC, abstractmethod
 
 class Animal(ABC):
-    
     @abstractmethod
     def sound(self):
         pass
 
 class Dog(Animal):
-    
     def sound(self):
-        return "Bark"
+        return f"Bark"
 
 class Cat(Animal):
-    
     def sound(self):
-        return "Meow"
+        return f"Meow"
