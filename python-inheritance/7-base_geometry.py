f7b92da52e257fb0a99885e8035539701d6b48c6
Damien353 <10034@holbertonstudents.com>
2025-02-06 09:36:46 +0000

    modif is not en isinstance

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -40,7 +40,7 @@ class BaseGeometry:
         superieur a 0, en levant les exceptions appropriees en cas d'echec
         de la validation.
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
