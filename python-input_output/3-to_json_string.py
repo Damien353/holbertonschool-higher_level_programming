74895d3ebe872bb35d5294036a53ef2933778228
Damien353 <10034@holbertonstudents.com>
2025-02-10 14:53:35 +0000

    test code

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Module qui contient une fonction permettant de retourner la representation JSON
+d'un objet Python sous forme de chaine de caracteres.
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Retourne la representation JSON d'un objet Python.
+
+    Args:
+        my_obj (object): L'objet Python a convertir en JSON.
+
+    Retourne:
+        str: La chaine JSON correspondant a l'objet fourni.
+
+    Cette fonction utilise la fonction json.dumps() pour convertir un objet Python
+    en une chaine JSON.
+    """
+    return json.dumps(my_obj)
