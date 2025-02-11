b3081365e0e930450e67e5138b99741982ac9c2f
Damien353 <10034@holbertonstudents.com>
2025-02-11 09:21:45 +0000

    modif import

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,23 +9,16 @@ nomme 'add_item.json'. Si le fichier n'existe pas, il est cree.
 
 import sys
 import json
-save_to_json_file = __import__('5-save_from_json_file')
-load_from_json_file = __import__('6-load_from_json_file')
+save_to_json_file = __import__('5-save_from_json_file').save_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def add_items():
-    """
-    Ajoute tous les arguments de la ligne de commande a une liste et la sauvegarde dans un fichier JSON.
+filename = "add_item.json"
 
-    Cette fonction :
-    - Charge la liste existante depuis le fichier JSON (si elle existe).
-    - Ajoute tous les nouveaux arguments de la ligne de commande a la liste.
-    - Sauvegarde la liste mise a jour dans le fichier JSON.
-    """
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
+try:
+items = load_from_json_file("add_item.json")
+except FileNotFoundError:
+items = []
 
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+items.extend(sys.argv[1:])
+save_to_json_file(items, "add_item.json")
