8a258db50989c412d3932b6f61fe8a6237fe0e15
Damien353 <10034@holbertonstudents.com>
2025-02-11 09:37:59 +0000

    inversion import

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,8 +7,8 @@ pour gerer la liste des arguments. La liste est sauvegardee dans un fichier JSON
 nomme 'add_item.json'. Si le fichier n'existe pas, il est cree.
 """
 
-import sys
 import json
+import sys
 
 
 save_to_json_file = __import__('5-save_from_json_file').save_from_json_file
@@ -18,9 +18,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 
 try:
-    items = load_from_json_file("filename")
+    items = load_from_json_file(filename)
 except FileNotFoundError:
     items = []
 
 items.extend(sys.argv[1:])
-save_to_json_file(items, "filename")
+save_to_json_file(items, filename)
