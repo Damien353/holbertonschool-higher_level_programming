1443436dc23ec962f804237dd91db4fbd109614c
Damien353 <10034@holbertonstudents.com>
2025-03-04 13:57:56 +0100

    Ajout tâche 1 et 2

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- Ce script crée la base de données hbtn_0c_0 sur le serveur MySQL
+-- Si la base de données existe déjà, elle ne sera pas recréée
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- Ce script supprime la base de données hbtn_0c_0 sur le serveur MySQL
+-- Si la base de données n'existe pas, le script ne génère pas d'erreur
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
