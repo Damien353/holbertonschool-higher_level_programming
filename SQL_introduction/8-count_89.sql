2fde2baba7fb26a035d1fd4edfe91b6434b1fc20
Damien353 <10034@holbertonstudents.com>
2025-03-05 11:02:19 +0100

    Test pour valider

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,7 @@
+-- Tâche : Ce script affiche le nombre d'enregistrements ayant id = 89 dans la table 'first_table' de la base de données spécifiée.
+-- 
+
+-- Compter le nombre de lignes où id = 89
+SELECT COUNT(*) 
+FROM first_table
+WHERE id = 89;
