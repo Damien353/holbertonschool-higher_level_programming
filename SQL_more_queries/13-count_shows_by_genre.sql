74100fbba0770c383f9d910a28ef6eaa7cfc7ba7
Damien353 <10034@holbertonstudents.com>
2025-03-05 16:29:36 +0100

    modif du ON, nom du group et ajout count

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,7 +4,7 @@
 SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM genres
 LEFT JOIN tv_show_genres
-ON tv_show_genres.genre_id = genres.id
-GROUP BY genres.name
-HAVING number_of_shows > 0
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.id
+HAVING COUNT(tv_show_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
