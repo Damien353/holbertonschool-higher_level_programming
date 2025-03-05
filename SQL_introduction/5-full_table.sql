2d39bd608608234c3676c936f2f7ea09d7c3ba1f
Damien353 <10034@holbertonstudents.com>
2025-03-05 10:58:13 +0100

    test pour tâche 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,8 @@
--- Tâche : Ce script affiche la description de la table 'first_table' dans la base de données spécifiée.
+-- Ce script affiche la description de la table 'first_table' dans la base de données spécifiée.
 -- La commande ne doit pas utiliser DESCRIBE ou EXPLAIN.
+
+-- Sélectionner la base de données avant d'exécuter la requête (optionnel)
+USE hbtn_0c_0;
 
 -- Sélectionner la définition de la table 'first_table' depuis information_schema.tables
 SELECT CONCAT('Table ', table_name, '    ', CREATE_TABLE) AS 'Table   Create Table'
