c5c9d35b339a31996c8cf54ec32cd943acb7519d
Damien353 <10034@holbertonstudents.com>
2025-03-05 16:20:05 +0100

    test à la ligne

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,7 +3,8 @@
 
 SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM genres
-LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = genres.id
+LEFT JOIN tv_show_genres
+ON tv_show_genres.genre_id = genres.id
 GROUP BY genres.id
 HAVING COUNT(tv_show_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
