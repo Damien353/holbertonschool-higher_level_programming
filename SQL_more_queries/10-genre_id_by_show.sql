014c8f1ce6eb325030e6ceb045d89c9b3f6aa492
Damien353 <10034@holbertonstudents.com>
2025-03-05 16:09:58 +0100

    S au mauvais endroit dans ON

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -4,5 +4,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_shows_genres.shows_id = tv_show.id
+ON tv_shows_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
