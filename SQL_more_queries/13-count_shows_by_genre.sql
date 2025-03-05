1369431bc4eeb11fcda05902bdfd94aca55646fc
Damien353 <10034@holbertonstudents.com>
2025-03-05 16:18:36 +0100

    test

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- Script pour lister les genres et le nombre de shows liés à chaque genre
+--
+
+SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM genres
+LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = genres.id
+GROUP BY genres.id
+HAVING COUNT(tv_show_genres.show_id) > 0
+ORDER BY number_of_shows DESC;
